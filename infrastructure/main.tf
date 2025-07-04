0fa14a13b464f7b441e2a0950675739a3f11b294
alkh0115 <alkh0115@algonquinlive.com>
2025-07-03 20:51:05 -0400

    fix: make terraform fmt fail correctly

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-name     = "example-resources"
-location = "East US"
-
+  name     = "example-resources"
+  location = "East US"
+}
